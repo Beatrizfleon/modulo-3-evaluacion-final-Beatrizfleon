6bfa5e6412f843c7ad35d50be523570e3a047f88
Beatrizfleon <Beatrizfleon@gmail.com>
2020-04-25 22:56:07 +0200

    run docs

---
+++ docs/precache-manifest.f507db44b08b36a750c117d643c495a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c78f00aa5de68abd11a73ca6861e3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f2933626b2d8b698b15",
+    "url": "./static/css/main.a84a18a6.chunk.css"
+  },
+  {
+    "revision": "0c0b93196f8b4561bc79",
+    "url": "./static/js/2.fb7f93e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.fb7f93e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2933626b2d8b698b15",
+    "url": "./static/js/main.4a96fb73.chunk.js"
+  },
+  {
+    "revision": "f675dd53d3d581e776da",
+    "url": "./static/js/runtime-main.17138bfb.js"
+  },
+  {
+    "revision": "a85a196836b0f281aca969ff48d7ca6b",
+    "url": "./static/media/Rick.a85a1968.png"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b0604b927273db8b7dfe73bfcee86e88.js"
+  "./precache-manifest.f507db44b08b36a750c117d643c495a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
